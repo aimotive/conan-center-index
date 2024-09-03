1fac1acaead691c99bdcf2297e5de03b02e4d207
Csaba Keszegh <csaba.keszegh@aimotive.com>
2024-09-03 14:23:30 +0200

    units: add aimotive specific patches
    Change-Id: Id4cd6d176c07052f55f922e4d8be910903737f5b

--- recipes/units/all/conanfile.py
+++ recipes/units/all/conanfile.py
@@ -1,9 +1,10 @@
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
-from conan.tools.files import copy, get
-from conan.tools.layout import basic_layout
+from conan.tools.files import copy, get, rmdir, export_conandata_patches, apply_conandata_patches
 from conan.tools.scm import Version
+from conan.tools.cmake import CMake
+from conan.tools.cmake.layout import cmake_layout
 import os
 
 required_conan_version = ">=1.50.0"
@@ -12,18 +13,18 @@ required_conan_version = ">=1.50.0"
 class UnitsConan(ConanFile):
     name = "units"
     description = (
-        "A compile-time, header-only, dimensional analysis and unit conversion "
+        "A compile-time, dimensional analysis and unit conversion "
         "library built on c++14 with no dependencies"
     )
     license = "MIT"
     topics = ("unit-conversion", "dimensional-analysis", "cpp14",
-              "template-metaprogramming", "compile-time", "header-only",
-              "no-dependencies")
+              "template-metaprogramming", "compile-time", "no-dependencies")
     homepage = "https://github.com/nholthaus/units"
     url = "https://github.com/conan-io/conan-center-index"
-    package_type = "header-library"
     settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
+    generators = "CMakeToolchain"
+
 
     @property
     def _min_cppstd(self):
@@ -39,7 +40,7 @@ class UnitsConan(ConanFile):
         }
 
     def layout(self):
-        basic_layout(self, src_folder="src")
+        cmake_layout(self, src_folder="src")
 
     def package_id(self):
         self.info.clear()
@@ -53,18 +54,27 @@ class UnitsConan(ConanFile):
                 f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support.",
             )
 
+    def export_sources(self):
+        export_conandata_patches(self)
+
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        if "aimotive" in self.version:
+            apply_conandata_patches(self)
 
     def build(self):
-        pass
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
 
     def package(self):
         copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
-        copy(self, "units.h", src=os.path.join(self.source_folder, "include"), dst=os.path.join(self.package_folder, "include"))
+        cmake = CMake(self)
+        cmake.install()
+        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
 
     def package_info(self):
         self.cpp_info.set_property("cmake_file_name", "units")
         self.cpp_info.set_property("cmake_target_name", "units::units")
-        self.cpp_info.bindirs = []
-        self.cpp_info.libdirs = []
+        self.cpp_info.libs = ["units"]
+        self.cpp_info.includedirs = ["include", "include/units"]
